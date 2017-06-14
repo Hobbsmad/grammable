f1faabd839336cb15331c29095faf9262dcfbf76
Madelyn Hobbs <madelynhobbs@Madelyns-MacBook-Pro.local>
2017-06-14 13:49:05 -0500

    fixing tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,9 +6,11 @@ FactoryGirl.define do
     password "secretPassword"
     password_confirmation "secretPassword"
   end
-  
+
   factory :gram do
     message "hello"
+    picture { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png'), 'image/png') }
+
     association :user
   end
 end
